6f2ba5d10bc4f7b5636d6604adcdd93ceb238f51
Ekaitz10 <esotoap19dw31@ikzubirimanteo.com>
2022-03-21 13:39:59 +0100

    Maven- 2022/03/21

---
+++ Stream/Maven_Test/simple_test/src/main/java/org/sonatype/mavenbook/simple_test/org/sonatype/mavenbook/simple_test/App.java
@@ -0,0 +1,12 @@
+package org.sonatype.mavenbook.simple_test.org.sonatype.mavenbook.simple_test;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
