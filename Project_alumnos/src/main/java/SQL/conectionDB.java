469eb07e63e92f2b8a4d5221a370922f627566bb
irimo138 <uiriarteur19dw31@ikzubirimanteo.com>
2022-03-23 22:30:46 +0100

    Avanzando proyecto

--- Project_alumnos/src/main/java/SQL/conectionDB.java
+++ Project_alumnos/src/main/java/SQL/conectionDB.java
@@ -2,16 +2,38 @@ package SQL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class conectionDB {
+	
 	public conectionDB(){
 		try {
 			
-		Connection conn = DriverManager.getConnection("jdbc:odbc:ARTICLES");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Java","root", "");
+		System.err.println("realizado");
+		
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM profesor");
+		
+		while(rs.next()) {
+			String RESULT_NOMBRE = rs.getString(1);
+			String RESULT_ANO = rs.getString("añoInicio");
+			String RESULT_ALUMNOS = rs.getString(3);
+			
+			System.out.println( RESULT_NOMBRE + RESULT_ANO + RESULT_ALUMNOS);
+			
+		}
+		
 		}catch(Exception e) {
 			e.getMessage();
+			
 		}
+	}
+	
+	public static void main(String[] args) {
+		conectionDB c1 = new conectionDB();
 	}
 
 }
---
+++ Project_alumnos/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Index</title>
+</head>
+<body>
+	<form action="addProfesor.java" method="POST">
+		<input type="text" name="nombre">
+		<input type="number" name="añoInicio">
+		<input type="text" name="nombre">
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+</html>
