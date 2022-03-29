660a326ffc27b176293da6a8bd63a2369beadbc7
Ekaitz10 <iibargurenca20dw@ikzubirimanteo.com>
2022-03-29 12:53:57 +0200

    Spring Demo

---
+++ demo_Test/src/main/java/com/example/demo/DemoTetstApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTetstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTetstApplication.class, args);
+	}
+
+}
