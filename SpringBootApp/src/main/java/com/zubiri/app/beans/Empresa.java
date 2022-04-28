4ea8186b40e41b064c5abbae83b630073fc747f4
Ekaitz10 <iibargurenca20dw@ikzubirimanteo.com>
2022-04-28 13:55:14 +0200

    Updated 2022/04/28

--- SpringBootApp/src/main/java/com/zubiri/app/beans/Empresa.java
+++ SpringBootApp/src/main/java/com/zubiri/app/beans/Empresa.java
@@ -1,5 +1,7 @@
 package com.zubiri.app.beans;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,7 +22,7 @@ import lombok.RequiredArgsConstructor;
 @Entity
 @Getter
 @Table(name="Empresa")
-public class Empresa {
+public class Empresa implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
